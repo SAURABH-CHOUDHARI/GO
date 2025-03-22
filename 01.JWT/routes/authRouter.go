e249dd0f719615986c97afc9e38adaa78a90a6f5
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-22 19:03:45 +0530

    AuthRouter

--- 01.JWT/routes/authRouter.go
+++ 01.JWT/routes/authRouter.go
@@ -1,1 +1,12 @@
 package routes
+
+import(
+	controller "01.JWT/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incoming Routes *gin.Engine){
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+
+}
