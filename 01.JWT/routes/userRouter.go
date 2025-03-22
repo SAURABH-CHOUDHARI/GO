91ff47cfcf16f426c6926c5811ef920d93164c07
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-03-22 19:03:52 +0530

    userRouter

--- 01.JWT/routes/userRouter.go
+++ 01.JWT/routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import(
+	controller "01.JWT/controllers"
+	"01.JWT/middlewares"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incoming Routes *gin.Engine){
+	incomingRoutes.Use(middlewares.Authenticate())
+	incomingRoutes.GET("users", controller.GetUsers())
+	incomingRoutes.GET("users/:user_id", controller.GetUser())
+}
